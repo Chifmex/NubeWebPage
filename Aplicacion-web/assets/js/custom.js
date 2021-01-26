cac7ab7a1411fd392e24126e2c52c44b0be7c19a
Carlos Pichardo <pichardovicencio@gmail.com>
2021-01-25 18:37:32 -0600

    Auto-generated commit

---
+++ Aplicacion-web/assets/js/custom.js
@@ -0,0 +1,21 @@
+function bigArea(x){
+    console.log("creciooooo");
+    x.classList.add("col-10");
+    x.classList.remove("col");
+}
+
+function normalArea(x){
+    console.log("pequeno");
+    x.classList.add("col");
+    x.classList.remove("col-10");
+}
+
+
+
+/*
+$("#sidebar-hide-btn").click(function() {
+    $('#sidebar').hide();
+    $('#mapDiv').removeClass('col-lg-6');
+    $('#mapDiv').addClass('col-lg-8');
+});
+*/
