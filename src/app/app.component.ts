bb7127cdfad3140e9a142dcb4394cdea2cf55a88
Carlos Pichardo <pichardovicencio@gmail.com>
2021-02-04 19:07:44 -0600

    Hexagonos y cambios finales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'Aplicacion-Web';
+  title = 'Fundación NuBe';
 
   ngOnInit(){  
     console.log("oninit")
+    var images_carrusel = document.getElementsByClassName("image_carousel") as HTMLCollectionOf<HTMLElement>;
+    console.log(images_carrusel);
+    for( var i = 1; i < images_carrusel.length; i++) {
+    console.log(document.documentElement.clientWidth*.439)
+    images_carrusel[i].style.maxHeight = (document.documentElement.clientWidth*.439)+"px";
+    console.log(images_carrusel[i])
+  }
+
+
+    if(document.documentElement.clientWidth < 752){
+      console.log("carr")
+      var carrusel = document.getElementsByClassName("carrousel_images") as HTMLCollectionOf<HTMLElement>;
+      var navbar = document.getElementsByClassName("navbar") as HTMLCollectionOf<HTMLElement>;
+      console.log(carrusel[0]);
+      carrusel[0].style.transform = navbar[0].offsetHeight+"px";
+      var navbarInic = document.getElementsByClassName("navbar");
+      navbarInic[0].classList.add("gray");
+    }else{
+      var carrusel = document.getElementsByClassName("carrousel_images") as HTMLCollectionOf<HTMLElement>;
+      carrusel[0].style.marginTop ="0px";
+    }
   }
 
    
--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,20 +2,40 @@
 
 function bigArea(x){
     console.log("creciooooo");
-    x.classList.add("col-10");
-    x.classList.remove("col");
-    var titulos = document.getElementsByClassName("title")
+    if(document.documentElement.clientWidth >752){
+      var casillas = document.getElementsByClassName("subpunto");
+    
+    for(i = 0; i < casillas.length; i++) {
+      casillas[i].classList.add("col-md-1");  
+      casillas[i].classList.remove("col-md-4");
+      }
+
+    x.classList.remove("col-md-1");
+    x.classList.add("col-md-10");
+    
+    
+    
+    var titulos = document.getElementsByClassName("title");
     for(i = 0; i < titulos.length; i++) {
         titulos[i].style.display = "none"
       }
     var subtitulos = x.getElementsByClassName("subtitle")
     subtitulos[0].style.display = "block"  
+    }
+    
 }
 
 function normalArea(x){
     console.log("pequeno");
-    x.classList.add("col");
-    x.classList.remove("col-10");
+    
+    var casillas = document.getElementsByClassName("subpunto");
+
+    for(i = 0; i < casillas.length; i++) {
+        casillas[i].classList.add("col-md-4");
+        casillas[i].classList.remove("col-md-1");
+      }
+
+    x.classList.remove("col-md-10");
     var titulos = document.getElementsByClassName("title")
     for(i = 0; i < titulos.length; i++) {
         titulos[i].style.display = "block"
@@ -34,6 +54,34 @@ function changeActive(x){
 }
 
 
+window.onload = function(){ }
+window.onresize = function(){ arreglarTamano()}
+
+
+function arreglarTamano(){
+  var images_carrusel = document.getElementsByClassName("image_carousel");
+  console.log(images_carrusel);
+  for(i = 1; i < images_carrusel.length; i++) {
+    console.log(document.documentElement.clientWidth*.439)
+    images_carrusel[i].style.maxHeight = (document.documentElement.clientWidth*.439)+"px";
+    console.log(images_carrusel[i])
+  }
+
+
+  if(document.documentElement.clientWidth < 752){
+    console.log("carr")
+    var carrusel = document.getElementsByClassName("carrousel_images");
+    var navbar = document.getElementsByClassName("navbar");
+    console.log(carrusel[0])
+    carrusel[0].style.marginTop = navbar[0].offsetHeight+"px";
+    var navbarInic = document.getElementsByClassName("navbar");
+    navbarInic[0].classList.add("gray");
+  }else{
+    var carrusel = document.getElementsByClassName("carrousel_images");
+    carrusel[0].style.marginTop ="0px";
+  }
+}
+
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
@@ -45,42 +93,63 @@ var aliados = document.getElementsByClassName("Aliados");
 var contacto = document.getElementsByClassName("contacto");
 var proyectos = document.getElementsByClassName("Proyectos");
 var nosotros = document.getElementsByClassName("nosotros");
+var hexagonos = document.getElementsByClassName("hexagonos");
+
 
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     var sticky = navbar[0].offsetTop+100;
-  if (window.pageYOffset >= sticky) {
-    navbar[0].classList.add("gray");
-  } else {
-    navbar[0].classList.remove("gray");
-  }
+  
+    if(document.documentElement.clientWidth > 752){
+      if (window.pageYOffset >= sticky) {
+        navbar[0].classList.add("gray");
+      } else {
+        navbar[0].classList.remove("gray");
+      }
+    }else{
+      navbar[0].classList.add("gray");
+    }
+  
 
-
-  if(window.pageYOffset < nosotros[0].offsetTop-753) {
+  if(window.pageYOffset-hexagonos[0].offsetTop < -90){
+    console.log("inicio")
+    console.log(window.pageYOffset-nosotros[0].offsetTop)
     var navbar_contacto = document.getElementsByClassName("nav_inicio"); 
     changeActive(navbar_contacto[0]);
     }
-  
-  console.log(proyectos[0].offsetTop);
-  console.log(window.pageYOffset)  
-  if((window.pageYOffset > proyectos[0].offsetTop-100) && (window.pageYOffset < proyectos[0].offsetTop+100)){
+
+    if((window.pageYOffset-hexagonos[0].offsetTop > -90 ) && (window.pageYOffset-hexagonos[0].offsetTop < 30)){
+      console.log("hexagonos")
+      console.log(window.pageYOffset-hexagonos[0].offsetTop)
+      var navbar_hexagonos = document.getElementsByClassName("nav_hexagonos"); 
+      changeActive(navbar_hexagonos[0]);
+    }
+
+  if((window.pageYOffset-proyectos[0].offsetTop > -50 ) && (window.pageYOffset-proyectos[0].offsetTop < 30)){
+    console.log("proyectos")
+    console.log(window.pageYOffset-proyectos[0].offsetTop)
     var navbar_proyectos = document.getElementsByClassName("nav_proyectos"); 
     changeActive(navbar_proyectos[0]);
   }
 
-  if((window.pageYOffset > nosotros[0].offsetTop-753) && (window.pageYOffset < nosotros[0].offsetTop+100)){
+  if((window.pageYOffset-nosotros[0].offsetTop > -150 ) && (window.pageYOffset-nosotros[0].offsetTop < 10)){
+    console.log("nosotros")
+    console.log(window.pageYOffset-nosotros[0].offsetTop)
     var navbar_nosotros = document.getElementsByClassName("nav_nosotros"); 
     changeActive(navbar_nosotros[0]);
   }
 
-  if((window.pageYOffset > aliados[0].offsetTop-800) && (window.pageYOffset < aliados[0].offsetTop+100)){
+  if((window.pageYOffset-aliados[0].offsetTop > -80 ) && (window.pageYOffset-aliados[0].offsetTop < 30)){
+    console.log("aliados")
+    console.log(window.pageYOffset-aliados[0].offsetTop)
     var navbar_aliados = document.getElementsByClassName("nav_aliados"); 
     changeActive(navbar_aliados[0]);
   }
 
 
-  if(window.pageYOffset > contacto[0].offsetTop-500) {
+  if(window.pageYOffset-contacto[0].offsetTop > -500) {
+    console.log("contactos")
     var navbar_contacto = document.getElementsByClassName("nav_contactos"); 
     changeActive(navbar_contacto[0]);
     }
